922bc1fa1c49d8f6a5fd6d2a7f380f5d6fbca28d
MaryHoy <55214178+MaryHoy@users.noreply.github.com>
2020-03-30 13:03:00 -0700

    add gifted chat and styling

--- components/Chat.js
+++ components/Chat.js
@@ -1,31 +1,76 @@
 import React, { Component } from 'react';
+import KeyboardSpacer from 'react-native-keyboard-spacer';
 //import components from react native
-import { StyleSheet, Text, Button, View } from 'react-native';
+import { StyleSheet, ImageBackground, Text, TextInput, Alert, TouchableOpacity, Button, View, Platform } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+
 
 export default class Chat extends React.Component {
-  //places users name in navigation bar
+  constructor(props){
+    super(props);
+    this.state = {
+      messages: [],
+    }
+  }
+
+
+  //this will put the users name in navigation bar
   static navigationOptions = ({ navigation }) =>{
     return {
       title: navigation.state.params.name,
     }
   }
+// renders system messages
+componentDidMount() {
+  this.setState({
+    messages: [
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+      {
+        _id: 2,
+        text: 'This is a system message',
+        createdAt: new Date(),
+        system: true,
+       },
+    ]
+  })
+}
+
+onSend(messages = []) {
+  this.setState(previousState => ({
+    messages: GiftedChat.append(previousState.messages, messages),
+  }))
+}
 
   //renders components
   render(){
-  return (
-    //fullscreen component
-    <View style={[styles.container, {backgroundColor: this.props.navigation.state.params.color}] }>
-      <Text style={{color: '#FFFFFF'}}>This is your chat screen</Text>
-    </View>
-  );
-}
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    return (
+      <View style={{flex: 1, backgroundColor: this.props.navigation.state.params.color}}>
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={messages => this.onSend(messages)}
+        user={{_id: 1,}}
+      />
+      {Platform.OS === 'android' ? <KeyboardSpacer /> : null }
+      </View>
+    );
+  }
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '100%',
+    },
+  });
